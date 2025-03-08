6ac417e3eaca5d54535331a3021340d3b0f5604e
Barnabas KolozJnr <67397553+kolozjnr@users.noreply.github.com>
2025-03-08 15:08:59 +0100

    added postgre data to configs

--- config/database.php
+++ config/database.php
@@ -84,12 +84,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DB_URL', 'postgresql://postgres:lTrKMcJOCsMZovZfoLimkCuDdwlgxBgl@postgres.railway.internal:5432/railway'),
+            'host' => env('DB_HOST', 'postgres.railway.internal'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'railway'),
+            'username' => env('DB_USERNAME', 'postgres'),
+            'password' => env('DB_PASSWORD', 'lTrKMcJOCsMZovZfoLimkCuDdwlgxBgl'),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
